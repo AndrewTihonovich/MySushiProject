d133c8404a72c2cf042ae3fab257331dbe4deba0
AndrewTihonovich <AndrewT_87@tut.by>
2021-08-17 17:05:03 +0300

    UI_Try_ConsoleColor

--- MySushiProject/Program.cs
+++ MySushiProject/Program.cs
@@ -1,12 +1,85 @@
-﻿using System;
+﻿using MySushiProject.UI;
+using System;
+using System.Collections.Generic;
 
 namespace MySushiProject
 {
     class Program
     {
+        static bool isEnter = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> Menu = new List<string>()
+            {
+                "Аризона",
+                "Атами",
+                "Сакура",
+                "Филадельфия",
+                "Мияги",
+                "Токио",
+            };
+
+            Console.WriteLine("Здравствуйте");
+            Console.WriteLine("Как Вас зовут?");
+            string name = " Console.ReadLine(); ";
+
+            Console.Clear();
+            Console.WriteLine($"{name}, Вы можете сделать заказ из меню на сегодня:");
+
+
+
+            (int X, int Y) positionStart = Console.GetCursorPosition();
+            foreach (var item in Menu)
+            {
+                Console.WriteLine(item);
+            }
+            (int X, int Y) positionStop = Console.GetCursorPosition();
+
+            Console.Clear();
+            Console.WriteLine($"{name}, Вы можете сделать заказ из меню на сегодня:");
+
+
+
+            string message = $"{name}, Вы можете сделать заказ из меню на сегодня:";
+            int count = Menu.Count;
+            int cursor = 0;
+            
+            while (!isEnter) //Console.ReadKey().Key != ConsoleKey.Enter
+            {
+                for (int i = positionStart.Y; i < positionStop.Y; i++)
+                {
+                        if (cursor == i- positionStart.Y)
+                        {
+                            Console.BackgroundColor = (ConsoleColor)12;
+                            Console.WriteLine(Menu[i - positionStart.Y]);
+                            Console.BackgroundColor = (ConsoleColor)0;
+                        }
+                        else
+                        {
+                            Console.WriteLine(Menu[i - positionStart.Y]);
+                        }
+                }
+
+                cursor = ChekButtons.CheckBut(cursor, out isEnter);
+
+                Console.Clear();
+                Console.WriteLine($"{message}");
+            }
+
+
+            
+
+
+
+
+
+
         }
+
+
+
+
+
     }
 }
+
---
+++ MySushiProject/UI/ChekButtons.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySushiProject.UI
+{
+    class ChekButtons
+    {
+        public static int CheckBut(int count, out bool but)
+        {
+            but = false;
+            var keyL = ConsoleKey.LeftArrow;
+            var keyR = ConsoleKey.RightArrow;
+            var keyU = ConsoleKey.UpArrow;
+            var keyD = ConsoleKey.DownArrow;
+            var keyEnt = ConsoleKey.Enter;
+            var keyEsc = ConsoleKey.Escape;
+
+            var key = Console.ReadKey();
+
+            if (key.Key == keyU)
+            {
+                count--;
+
+            }
+
+            if (key.Key == keyD)
+            {
+                count++;
+
+            }
+
+            if (key.Key == keyEnt)
+            {
+
+                but = true;
+
+            }
+
+
+            return count;
+        }
+    }
+}
